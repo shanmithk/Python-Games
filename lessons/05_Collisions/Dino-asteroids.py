959beb33df7281cf6f331da9df6208e05248ef7e
shanmithk <kadhome31@gmail.com>
2025-05-25 18:25:49 +0000

    h

--- lessons/05_Collisions/Dino-asteroids.py
+++ lessons/05_Collisions/Dino-asteroids.py
@@ -113,7 +113,7 @@ class Projectile(pygame.sprite.Sprite):
     def __init__(self, x,y):
         super().__init__()
         self.image = pygame.Surface((OBSTACLE_WIDTH, OBSTACLE_HEIGHT))
-        self.image.fill(BLACK)
+        self.image.fill(BLACK)  
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
